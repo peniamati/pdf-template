f8a0171029c3fefa62c5f6c10f1faaaf7504b8b8
peniamati <pena_matias@hotmail.com>
2023-03-22 16:30:03 -0300

    Exercise: Add lines

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
              ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
 
     #Set the footer
     pdf.ln(265)
@@ -26,13 +28,16 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
+        for i in range(20, 298, 10):
+            pdf.line(10, i, 200, i)
+
         # Set the footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
-pdf.output("output.pdf")
+pdf.output("output_lined.pdf")
 
 
 
